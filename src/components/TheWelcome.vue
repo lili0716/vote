d18af55c78f189a18bddd7709865533056ed7570
lili <yccy7788@163.com>
2026-01-19 15:43:41 +0800

    Initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>Welcome to Your Vue.js App</h1>
+    <p>This is a simple welcome component.</p>
+    <div class="item-container">
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+.container {
+  text-align: center;
+  background-color: red;
+  color: white;
+  padding: 2rem;
+}
+</style>
